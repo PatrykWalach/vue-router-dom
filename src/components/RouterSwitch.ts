f7716d3bfa22969c919fdc36678dc13123e3cbc6
PatrykWalach <patryk_walach@outlook.com>
2020-02-24 17:37:22 +0100

    fix: check for location

--- src/components/RouterSwitch.ts
+++ src/components/RouterSwitch.ts
@@ -1,31 +1,29 @@
+import { computed, defineComponent } from 'vue'
 import { Path } from 'history'
-import { defineComponent } from 'vue'
 import { matchPath } from '../matchPath'
 import { useLocation } from '../hooks/useLocation'
 
 export interface RouterSwitchProps {
-  location: Path
+  location: Path | null
 }
 
 export const RouterSwitch = defineComponent({
   name: 'RouterSwitch',
   props: { location: { default: null, required: false, type: null } },
-  setup(switchProps: Readonly<RouterSwitchProps>, { slots }) {
+  setup(props: Readonly<RouterSwitchProps>, { slots }) {
     const location = useLocation()
+    const pathname = computed(() => props.location && props.location.pathname)
 
     return () =>
       slots.default().find(({ props }) => {
         const path = (props && (props.path || props.from)) || ''
         const exact = (props && props.exact) || false
         const strict = (props && props.strict) || false
-        return matchPath(
-          switchProps.location.pathname && location.value.pathname,
-          {
-            exact,
-            path,
-            strict,
-          },
-        )
+        return matchPath(pathname.value || location.value.pathname, {
+          exact,
+          path,
+          strict,
+        })
       })
   },
 })
