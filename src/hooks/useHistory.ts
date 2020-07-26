36dd8813f2cd38f99657d581873fef193cd4e014
PatrykWalach <patryk_walach@outlook.com>
2020-07-26 12:39:14 +0200

    fix: useHistory types

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,9 +1,11 @@
-import { MemoryHistory, History } from 'history'
+import { MemoryHistory, History, BrowserHistory, HashHistory } from 'history'
 import { ROUTER_HISTORY } from '../api/keys'
 import { assert } from '../utils/assert'
 import { inject } from 'vue'
 
-export const useHistory = <T extends History | MemoryHistory = History>() => {
+export const useHistory = <
+  T extends BrowserHistory | HashHistory | MemoryHistory = BrowserHistory
+>() => {
   const history = inject<T>(ROUTER_HISTORY)
 
   assert(history, `no history provided to injection`)
